daa07d2ec072b148fdebb431f4e7a6defe4b382c
Christian Schenk <cs@miktex.org>
2017-05-10 15:07:12 +0200

    Print formula info

--- travis-ci/script.sh
+++ travis-ci/script.sh
@@ -3,5 +3,7 @@
 set -ev
 
 brew tap miktex/miktex "${TRAVIS_BUILD_DIR}"
+brew install jq
+brew info --json=v1 miktex-bare | jq .
 brew install --verbose --build-bottle --devel miktex-bare
 brew bottle --verbose miktex-bare
