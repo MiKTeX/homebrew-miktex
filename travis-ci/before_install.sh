e2b141be33ceffe47dd7083e4d59d6567c724136
Christian Schenk <cs@miktex.org>
2017-05-14 21:15:05 +0200

    Run test suite

--- travis-ci/before_install.sh
+++ travis-ci/before_install.sh
@@ -6,3 +6,4 @@ brew update
 
 ( brew ls -1 | grep -w ghostscript ) || brew install ghostscript
 ( brew ls -1 | grep -w md5sha1sum ) || brew install md5sha1sum
+( brew ls -1 | grep -w pandoc ) || brew install pandoc
