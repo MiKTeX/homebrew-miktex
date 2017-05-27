13abbc85881ac9b9573acd1b90ef9c9a347c16d4
Christian Schenk <cs@miktex.org>
2017-05-27 09:39:59 +0200

    MiKTeX 2.9.6341

--- Formula/miktex.rb
+++ Formula/miktex.rb
@@ -1,20 +1,20 @@
 class Miktex < Formula
   desc "TeX & Friends"
   homepage "https://miktex.org/"
-  url "http://download.miktex.org/tmp/78F6358B2D024188A7B7CCF55EB7E8BE/miktex-2.9.6340.tar.xz"
-  sha256 "ecf403a35dfd70b5e9826ce2e551bd93d5b144de09f720ffe39df5e6bf418988"
+  url "https://miktex.org/download/ctan/systems/win32/miktex/source/miktex-2.9.6341.tar.xz"
+  sha256 "1e53d3dc69adb7645aeac3b341c5d80970ce15de3177e0d246d7057659f2d172"
 
   devel do
     url "https://github.com/MiKTeX/miktex.git", :branch => "next"
     version "2.9-next"
   end
 
-  bottle do
-    root_url "https://dl.bintray.com/miktex/bottles"
-    sha256 "d651052bc5bc81cbc1e51f87e2afeb4ddb292d04d4ba7e28bc6368f50a98aee7" => :sierra
-    sha256 "dc1a6ff2d897dfe89f561c0251b170615176d171b16d9363e683edd55eafdf1a" => :el_capitan
-    sha256 "3de88963cd3f110263faf32825ca473579724b4149364a9a6044f4c74b818a65" => :yosemite
-  end
+#  bottle do
+#    root_url "https://dl.bintray.com/miktex/bottles"
+#    sha256 "d651052bc5bc81cbc1e51f87e2afeb4ddb292d04d4ba7e28bc6368f50a98aee7" => :sierra
+#    sha256 "dc1a6ff2d897dfe89f561c0251b170615176d171b16d9363e683edd55eafdf1a" => :el_capitan
+#    sha256 "3de88963cd3f110263faf32825ca473579724b4149364a9a6044f4c74b818a65" => :yosemite
+#  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
